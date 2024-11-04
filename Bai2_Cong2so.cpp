5d1c29d321c8f6b3f7126ad1137de998e293e53d
NguyenPhuThanh29082008 <nguyenphuthanhpch298@gmail.com>
2024-11-04 14:32:57 +0700

    Dùng Cin để cộng trừ nhân chia các số

--- Bai2_Cong2so.cpp
+++ Bai2_Cong2so.cpp
@@ -4,14 +4,26 @@ using namespace std;
 int main()
 {
     cout << "CHUONG TRINH TONG VA TICH 3 SO" << endl;
-    int so1, so2, so3;
-    int tongvatich;
-    so1=10;
-    so2=2;
-    so3=5;
-    tongvatich = so1+so2*so3;
-    cout << "Tong va tich 3 so: " << so1 << "+" << so2 << "*" << so3 << "=" << tongvatich;
+    cout << "ta co bieu thuc a+b*c\n";
+    int a, b, c, d, e, f, g;          //Khai bao bien so1 va so2 kieu so nguyen
+    int tongvatich;        //Khai bao tong
+    int h;
+    cout << "nhap so a: "; cin >> a;
+    cout << "nhap so b: "; cin >> b;
+    cout << "nhap so c: "; cin >> c;
+    tongvatich = a+b*c;
+
+    cout << "Tong va tich 3 so: " << a << "+" << b << "*" << c << "=" << tongvatich << endl;
+
+    cout << "nhap so e: "; cin >> e;
+    f = tongvatich + e;
+    cout << "ta co so f = tongvatich + so e\n";
+    cout << "so f = " << tongvatich << "+" << e << "=" << f << endl;
+
+    cout << "nhap so g: "; cin >> g;
+    h = f / g;
+    cout << "ta cho h = f : g thi ta se co:\n";
+    cout << "so h = " << f << " : " << g << " = " << h << endl;
+
     return 0;
 }
-
-
