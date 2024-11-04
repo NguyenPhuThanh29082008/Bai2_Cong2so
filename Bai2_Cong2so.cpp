c3bb230a3fd5b58a98489ea165fc15fd292a38c4
NPThanh <nguyenphuthanhpch298@gmail.com>
2024-11-04 16:42:43 +0700

    Update Bai2_Cong2so.cpp

--- Bai2_Cong2so.cpp
+++ Bai2_Cong2so.cpp
@@ -5,9 +5,9 @@ int main()
 {
     cout << "CHUONG TRINH TONG VA TICH 3 SO" << endl;
     cout << "ta co bieu thuc a+b*c\n";
-    int a, b, c, d, e, f, g;          //Khai bao bien so1 va so2 kieu so nguyen
-    int tongvatich;        //Khai bao tong
-    int h;
+    int a, b, c, d, e, f, g;          //Khai bao bien a b c d e f g kieu so nguyen
+    int tongvatich;        //Khai bao tongvatich
+    int h;                 //Khai bao h
     cout << "nhap so a: "; cin >> a;
     cout << "nhap so b: "; cin >> b;
     cout << "nhap so c: "; cin >> c;
