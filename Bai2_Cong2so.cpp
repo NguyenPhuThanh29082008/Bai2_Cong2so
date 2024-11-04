3c4e51c6ea405c679dc6ba4920f467e09a04da1e
NguyenPhuThanh29082008 <nguyenphuthanhpch298@gmail.com>
2024-11-04 13:33:08 +0700

    Add project files.

---
+++ Bai2_Cong2so.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "CHUONG TRINH TONG VA TICH 3 SO" << endl;
+    int so1, so2, so3;
+    int tongvatich;
+    so1=10;
+    so2=2;
+    so3=5;
+    tongvatich = so1+so2*so3;
+    cout << "Tong va tich 3 so: " << so1 << "+" << so2 << "*" << so3 << "=" << tongvatich;
+    return 0;
+}
+
+
